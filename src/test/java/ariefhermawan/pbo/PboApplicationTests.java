de50798bc91e3d792ca6a4e7b2093fd1485ea4b1
Arief-Karditya <nolifegansaya@gmail.com>
2024-10-06 10:49:06 +0700

    Membuat Project

---
+++ src/test/java/ariefhermawan/pbo/PboApplicationTests.java
@@ -0,0 +1,13 @@
+package ariefhermawan.pbo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
